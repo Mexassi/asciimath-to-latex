c5bb6b545ff77e68a868bf2147ff08b730456695
Massimo Presta <massimo.presta@smartsparrow.com>
2019-09-30 15:54:51 +1000

    do not trim empty spaces

--- index.js
+++ index.js
@@ -338,9 +338,11 @@ function newsymbol(symbolobj) {
 function AMremoveCharsAndBlanks(str, n) {
   //remove n characters and any following blanks
   var st;
-  if (str.charAt(n) == "\\" && str.charAt(n + 1) != "\\" && str.charAt(n + 1) != " ")
+  if (str.charAt(n) == "\\" && str.charAt(n + 1) != "\\") {
     st = str.slice(n + 1);
-  else st = str.slice(n);
+  } else {
+    st = str.slice(n);
+  } 
   for (var i = 0; i < st.length && st.charCodeAt(i) <= 32; i = i + 1);
   return st.slice(i);
 }
